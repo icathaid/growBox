3a1509001b478b7f0a25bed0a9d66b62b1929328
James McDaniel <icathaid@gmail.com>
2018-12-01 13:25:07 -0800

    function/object import/export syntax fixed

--- dist/dist/bundle.js
+++ dist/dist/bundle.js
@@ -94,7 +94,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _lights_lights_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lights/lights.js */ \"./src/lights/lights.js\");\n\n\nconsole.log(_lights_lights_js__WEBPACK_IMPORTED_MODULE_0__[\"person\"].name);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvZ3Jvdy5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3NyYy9ncm93LmpzPzA3ODQiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgcGVyc29uIH0gZnJvbSAnLi9saWdodHMvbGlnaHRzLmpzJztcblxuY29uc29sZS5sb2cocGVyc29uLm5hbWUpOyJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQ0E7QUFDQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/grow.js\n");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _lights_lights_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lights/lights.js */ \"./src/lights/lights.js\");\n/* harmony import */ var _lights_lights_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_lights_lights_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _water_water_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./water/water.js */ \"./src/water/water.js\");\n\n\nconsole.log('HEY LOOK AT ME');\nconsole.log(_lights_lights_js__WEBPACK_IMPORTED_MODULE_0__[\"person\"].name);\n\nObject(_water_water_js__WEBPACK_IMPORTED_MODULE_1__[\"water\"])();//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvZ3Jvdy5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3NyYy9ncm93LmpzPzA3ODQiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHtwZXJzb259IGZyb20gJy4vbGlnaHRzL2xpZ2h0cy5qcyc7XG5pbXBvcnQge3dhdGVyfSBmcm9tICcuL3dhdGVyL3dhdGVyLmpzJztcbmNvbnNvbGUubG9nKCdIRVkgTE9PSyBBVCBNRScpO1xuY29uc29sZS5sb2cocGVyc29uLm5hbWUpO1xuXG53YXRlcigpOyJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/grow.js\n");
 
 /***/ }),
 
@@ -102,11 +102,22 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _lig
 /*!******************************!*\
   !*** ./src/lights/lights.js ***!
   \******************************/
-/*! exports provided: person */
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("let person = {\n  name: 'bill'\n};\n\nmodule.exports = {person};//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvbGlnaHRzL2xpZ2h0cy5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3NyYy9saWdodHMvbGlnaHRzLmpzPzc2ZjgiXSwic291cmNlc0NvbnRlbnQiOlsibGV0IHBlcnNvbiA9IHtcbiAgbmFtZTogJ2JpbGwnXG59O1xuXG5tb2R1bGUuZXhwb3J0cyA9IHtwZXJzb259OyJdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/lights/lights.js\n");
+
+/***/ }),
+
+/***/ "./src/water/water.js":
+/*!****************************!*\
+  !*** ./src/water/water.js ***!
+  \****************************/
+/*! exports provided: water */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"person\", function() { return person; });\nlet person = {\n  \"name\":\"bill\"\n};\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvbGlnaHRzL2xpZ2h0cy5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3NyYy9saWdodHMvbGlnaHRzLmpzPzc2ZjgiXSwic291cmNlc0NvbnRlbnQiOlsibGV0IHBlcnNvbiA9IHtcbiAgXCJuYW1lXCI6XCJiaWxsXCJcbn07XG5cbmV4cG9ydCB7IHBlcnNvbiB9OyJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQ0E7QUFDQTtBQUNBOyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/lights/lights.js\n");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"water\", function() { return water; });\nfunction water(){\n  return console.log('water running');\n}\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvd2F0ZXIvd2F0ZXIuanMuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvd2F0ZXIvd2F0ZXIuanM/OGMyNSJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiB3YXRlcigpe1xuICByZXR1cm4gY29uc29sZS5sb2coJ3dhdGVyIHJ1bm5pbmcnKTtcbn1cblxuZXhwb3J0IHt3YXRlcn07Il0sIm1hcHBpbmdzIjoiQUFBQTtBQUFBO0FBQUE7QUFDQTtBQUNBO0FBQ0E7Iiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/water/water.js\n");
 
 /***/ })
 
